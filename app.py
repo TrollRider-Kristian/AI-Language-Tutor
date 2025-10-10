181a6cde4d66cb685aee9b8a516ab90549428c4d
Kristian <Kristian.E64@gmail.com>
2025-10-10 14:07:12 +0200

    added some logging

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from prompt_engineer_dataset_creator import create_topic_list_from_prompt #, test_prompts_on_console
 from flask import Flask, render_template
+import logging
+from logging import FileHandler
 import ollama
+
 # from huggingface_attempt import run_huggingface_model # KRISTIAN_TODO - Why does it segfault?
+
 app = Flask(__name__)
 
 # @app.route('/')
 # def index():
 #     return 'placeholder'
+
+def initialize_logger():
+    logger = FileHandler ('debug.log')
+    logger.setLevel (logging.DEBUG)
+    app.logger.setLevel (logging.DEBUG)
+    app.logger.addHandler (logger)
+
+def write_response_to_log(response):
+    app.logger.debug (response)
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
@@ -14,6 +27,9 @@ def index():
 
 def main():
     # test_prompts_on_console()
+    initialize_logger()
+    response = create_topic_list_from_prompt (ollama.Client(), "llama3.1")
+    write_response_to_log (response.response)
     app.run (debug = True)  # KRISTIAN_TODO - Make a nice favicon for my new app.
 
 if __name__ == "__main__":
